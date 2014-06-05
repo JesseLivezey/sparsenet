1688c6627a1473265280bd05e41246f5496b0170
Jesse Livezey <jlivezey@audience.com>
2014-06-04 17:10:49 -0700

    changed eigsh to eigh

--- fista.py
+++ fista.py
@@ -19,7 +19,8 @@ def fista(I, Phi, lambdav, max_iterations=150, display=False):
   Q = Phi.T.dot(Phi)
   c = -2*Phi.T.dot(I)
 
-  L = scipy.sparse.linalg.eigsh(2*Q, 1, which='LM')[0]
+  last_eig = Q.shape[0]-1
+  L = scipy.linalg.eigh(2*Q, eigvals_only=True, eigvals=(last_eig,last_eig))[0]
   invL = 1/float(L)
 
   y = x
